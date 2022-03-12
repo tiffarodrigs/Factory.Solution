0cb825f35195f4945b249687924f5564a61daac4
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-03-11 16:15:49 -0800

    Add Client edit and delete  functionality

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
@@ -38,5 +39,36 @@ namespace HairSalon.Controllers
       Client thisClient = _db.Clients.FirstOrDefault(c => c.ClientId == id);
       return View(thisClient);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(c => c.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    } 
+
+    public ActionResult Delete(int id)
+    {
+    var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+    return View(thisClient);
+    }  
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+    var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+    _db.Clients.Remove(thisClient);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+    } 
+
+
   }
 }
