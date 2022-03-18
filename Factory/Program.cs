95931039c9cd8d71463fab85cb5fd6851e9ebc7d
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-03-18 09:01:06 -0700

    commit initial filesetup

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,7 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
-namespace HairSalon
+namespace Factory
 {
   public class Program
   {
--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using HairSalon.Models;
+using Factory.Models;
 
-namespace HairSalon
+namespace Factory
 {
   public class Startup
   {
@@ -26,7 +26,7 @@ namespace HairSalon
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<HairSalonContext>(options => options
+        .AddDbContext<FactoryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
