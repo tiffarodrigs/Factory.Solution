e2ada7c08171b93280a022b2a24ff012370d849f
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-03-18 11:16:09 -0700

    Complete index view in Engineer

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -15,6 +15,12 @@ namespace Factory.Controllers
     {
       _db = db;
     }
+      
+      public ActionResult Index()
+      {
+        List<Engineer> model = _db.Engineers.ToList();
+        return View(model);
+      }
   }
     
 } 
